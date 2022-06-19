93b4f94935102cf9c1e184afe33f9d89dea7e066
naoki1117 <105569021+naoki1117@users.noreply.github.com>
2022-06-19 22:29:35 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,105 @@
+import './style.css';
+import * as THREE from "three";
+import * as dat from "lil-gui";
+
+const gui = new dat.GUI();
+
+const canvas =document.querySelector(".webgl")
+const scene = new THREE.Scene();
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width/sizes.height,
+  0.1,
+  100
+);
+camera.position.z=6;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas,
+  alpha:true,
+});
+
+const material = new THREE.MeshPhysicalMaterial({
+  color:"#3c94d7",
+  metalness:0.86,
+  roughness:0.37,
+  flatShading:false,
+});
+
+gui.addColor(material,"color");
+gui.add(material,"metalness").min(0).max(1).step(0.01);
+gui.add(material,"roughness").min(0).max(1).step(0.01);
+
+const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1,0.4,16,60),material);
+const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(),material);
+const mesh3 = new THREE.Mesh(new THREE.TorusKnotGeometry(0.8,0.35,100,16),material);
+const mesh4 = new THREE.Mesh(new THREE.IcosahedronGeometry(),material);
+
+mesh1.position.set(2,0,0);
+mesh2.position.set(-1,0,0);
+mesh3.position.set(2,0,-6);
+mesh4.position.set(5,0,3);
+
+scene.add(mesh1,mesh2,mesh3,mesh4);
+const meshes =[mesh1,mesh2,mesh3,mesh4]
+
+const directionalLight = new THREE.DirectionalLight("#ffffff",4);
+directionalLight.position.set(0.5,1,0)
+scene.add(directionalLight);
+
+renderer.setSize(sizes.width,sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+window.addEventListener("resize",()=>{
+  sizes.width =innerWidth;
+  sizes.height=innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width,sizes.height);
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+let speed = 0;
+let rotation = 0;
+window.addEventListener("wheel", (event) => {
+  speed += event.deltaY * 0.0002
+  console.log(speed)
+});
+
+function rot(){
+  rotation += speed;
+  // speed *= 0.93;
+  mesh1.position.x = speed;
+  window.requestAnimationFrame(rot);
+}
+
+rot();
+
+const clock = new THREE.Clock();
+
+const animate = () => {
+  renderer.render(scene,camera)
+
+  let getDeltaTime = clock.getDelta();
+  
+  for (const mesh of meshes){
+    mesh.rotation.x +=0.1*getDeltaTime;
+    mesh.rotation.y +=0.12*getDeltaTime;
+  }
+  
+  
+  
+  
+  window.requestAnimationFrame(animate);
+
+};
+
+animate();
+
